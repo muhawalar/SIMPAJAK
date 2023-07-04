380007a3d984f0f08b95608a390f9fe9e549c379
muhawalar <awalarifin1@gmail.com>
2023-07-04 21:32:42 +0800

    make it 3 card to all report

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,7 +7,7 @@
               
             </div>
             <div class="d-xl-flex justify-content-between align-items-start">
-              <h4 class="mb-2 mt-3"> Sistem Manajemen Informasi Pajak Kabupaten BONE </h2>
+             
               
             </div>
             <div class="row">
@@ -16,47 +16,33 @@
                 <div class="tab-content tab-transparent-content">
                   <div class="tab-pane fade show active" id="business-1" role="tabpanel" aria-labelledby="business-tab">
                     <div class="row">
-                      <div class="col-xl-3 col-lg-6 col-sm-6 grid-margin stretch-card">
+                      <div class="col-xl-4 col-lg-6 col-sm-6 grid-margin stretch-card">
                         <div class="card">
                           <div class="card-body text-center">
-                            <h5 class="mb-2 text-dark font-weight-normal">Orders</h5>
-                            <h2 class="mb-4 text-dark font-weight-bold">932.00</h2>
-                            <div class="dashboard-progress dashboard-progress-1 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-lightbulb icon-md absolute-center text-dark"></i></div>
-                            <p class="mt-4 mb-0">Completed</p>
-                            <h3 class="mb-0 font-weight-bold mt-2 text-dark">5443</h3>
+                            <h3 class="mb-2 text-dark font-weight-normal">Wajib Pajak</h3>
+                            <h1 class="mb-5 mt-5 text-dark font-weight-bold">932.00</h1>
+                            <div class="dashboard-progress dashboard-progress-1 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-domain icon-md absolute-center text-dark mt-5"></i></div>
+                            <p class="mt-4 mb-0">Perusahaan</p>
                           </div>
                         </div>
                       </div>
-                      <div class="col-xl-3 col-lg-6 col-sm-6 grid-margin stretch-card">
+                      <div class="col-xl-4 col-lg-6 col-sm-6 grid-margin stretch-card">
                         <div class="card">
                           <div class="card-body text-center">
-                            <h5 class="mb-2 text-dark font-weight-normal">Unique Visitors</h5>
-                            <h2 class="mb-4 text-dark font-weight-bold">756,00</h2>
-                            <div class="dashboard-progress dashboard-progress-2 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-account-circle icon-md absolute-center text-dark"></i></div>
-                            <p class="mt-4 mb-0">Increased since yesterday</p>
-                            <h3 class="mb-0 font-weight-bold mt-2 text-dark">50%</h3>
+                          <h3 class="mb-2 text-dark font-weight-normal">Pembayaran</h3>
+                            <h1 class="mb-5 mt-5 text-dark font-weight-bold">932.00</h1>
+                            <div class="dashboard-progress dashboard-progress-1 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-domain icon-md absolute-center text-dark mt-5"></i></div>
+                            <p class="mt-4 mb-0">Perusahaan</p>
                           </div>
                         </div>
                       </div>
-                      <div class="col-xl-3  col-lg-6 col-sm-6 grid-margin stretch-card">
+                      <div class="col-xl-4  col-lg-6 col-sm-6 grid-margin stretch-card">
                         <div class="card">
                           <div class="card-body text-center">
-                            <h5 class="mb-2 text-dark font-weight-normal">Impressions</h5>
-                            <h2 class="mb-4 text-dark font-weight-bold">100,38</h2>
-                            <div class="dashboard-progress dashboard-progress-3 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-eye icon-md absolute-center text-dark"></i></div>
-                            <p class="mt-4 mb-0">Increased since yesterday</p>
-                            <h3 class="mb-0 font-weight-bold mt-2 text-dark">35%</h3>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-xl-3 col-lg-6 col-sm-6 grid-margin stretch-card">
-                        <div class="card">
-                          <div class="card-body text-center">
-                            <h5 class="mb-2 text-dark font-weight-normal">Followers</h5>
-                            <h2 class="mb-4 text-dark font-weight-bold">4250k</h2>
-                            <div class="dashboard-progress dashboard-progress-4 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-cube icon-md absolute-center text-dark"></i></div>
-                            <p class="mt-4 mb-0">Decreased since yesterday</p>
-                            <h3 class="mb-0 font-weight-bold mt-2 text-dark">25%</h3>
+                          <h3 class="mb-2 text-dark font-weight-normal">Tunggakan</h3>
+                            <h1 class="mb-5 mt-5 text-dark font-weight-bold">932.00</h1>
+                            <div class="dashboard-progress dashboard-progress-1 d-flex align-items-center justify-content-center item-parent"><i class="mdi mdi-domain icon-md absolute-center text-dark mt-5"></i></div>
+                            <p class="mt-4 mb-0">Perusahaan</p>
                           </div>
                         </div>
                       </div>
