ebc48c1b62c6a979b4e362f3a65fc610285cc48e
muhawalar <awalarifin1@gmail.com>
2023-07-26 09:20:30 +0800

    change datatables syntax

--- public/js/data-tables.js
+++ public/js/data-tables.js
@@ -1,9 +1,6 @@
-// $(document).ready(function () {
-//     $('#wajibPajakData').DataTable();
-//     $('.dataTables_length').addClass('bs-select');
-//     });
-new DataTable('#wajibPajakData');
 $(document).ready(function () {
-    $('wajibPajakData').DataTable();
-  });
+    $('#wajibPajakData').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+    });
+
   
