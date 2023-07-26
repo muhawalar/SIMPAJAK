365e1f99225016ced62a2d0694a4f7dda3fc7623
muhawalar <awalarifin1@gmail.com>
2023-07-26 09:14:32 +0800

    try to fixing datatables

--- public/js/data-tables.js
+++ public/js/data-tables.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
-    $('#wajibPajakData').DataTable();
-    $('.dataTables_length').addClass('bs-select');
-    });
+// $(document).ready(function () {
+//     $('#wajibPajakData').DataTable();
+//     $('.dataTables_length').addClass('bs-select');
+//     });
 new DataTable('#wajibPajakData');
 $(document).ready(function () {
     $('wajibPajakData').DataTable();
